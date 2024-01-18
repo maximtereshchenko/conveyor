5ad3bb0317d20cfb4d578f25835d74acd6e61267
maksimt_backbase <maksimt@backbase.com>
2024-01-18 16:59:58 +0100

    Inherit dependencies from parent

--- conveyor-domain/src/main/java/com/github/maximtereshchenko/conveyor/domain/Parent.java
+++ conveyor-domain/src/main/java/com/github/maximtereshchenko/conveyor/domain/Parent.java
@@ -1,9 +1,12 @@
 package com.github.maximtereshchenko.conveyor.domain;
 
+import com.github.maximtereshchenko.conveyor.api.port.DependencyDefinition;
 import com.github.maximtereshchenko.conveyor.api.port.PluginDefinition;
 import java.util.Collection;
 
 interface Parent {
 
     Collection<PluginDefinition> plugins();
+
+    Collection<DependencyDefinition> dependencies();
 }
--- conveyor-domain/src/main/java/com/github/maximtereshchenko/conveyor/domain/SuperParent.java
+++ conveyor-domain/src/main/java/com/github/maximtereshchenko/conveyor/domain/SuperParent.java
@@ -1,25 +1,31 @@
 package com.github.maximtereshchenko.conveyor.domain;
 
 import com.github.maximtereshchenko.conveyor.api.port.ArtifactDefinition;
+import com.github.maximtereshchenko.conveyor.api.port.DependencyDefinition;
 import com.github.maximtereshchenko.conveyor.api.port.PluginDefinition;
+import com.github.maximtereshchenko.conveyor.api.port.ProjectDefinition;
 import java.util.Collection;
-import java.util.List;
 
 final class SuperParent implements Parent {
 
-    private final Collection<PluginDefinition> plugins;
+    private final ProjectDefinition projectDefinition;
 
-    SuperParent(Collection<PluginDefinition> plugins) {
-        this.plugins = List.copyOf(plugins);
+    SuperParent(ProjectDefinition projectDefinition) {
+        this.projectDefinition = projectDefinition;
     }
 
     static Parent from(DirectoryRepository repository) {
-        return new SuperParent(repository.projectDefinition(new SuperParentArtifactDefinition()).plugins());
+        return new SuperParent(repository.projectDefinition(new SuperParentArtifactDefinition()));
     }
 
     @Override
     public Collection<PluginDefinition> plugins() {
-        return plugins;
+        return projectDefinition.plugins();
+    }
+
+    @Override
+    public Collection<DependencyDefinition> dependencies() {
+        return projectDefinition.dependencies();
     }
 
     private static final class SuperParentArtifactDefinition implements ArtifactDefinition {
