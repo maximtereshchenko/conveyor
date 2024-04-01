cb29abd1acd8dd68c450cbd99543b4e4a8eecd6c
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-04-01 21:30:22 +0200

    Inherit properties from parent

--- conveyor-domain/src/main/java/com/github/maximtereshchenko/conveyor/domain/Parent.java
+++ conveyor-domain/src/main/java/com/github/maximtereshchenko/conveyor/domain/Parent.java
@@ -3,10 +3,13 @@ package com.github.maximtereshchenko.conveyor.domain;
 import com.github.maximtereshchenko.conveyor.api.port.DependencyDefinition;
 import com.github.maximtereshchenko.conveyor.api.port.PluginDefinition;
 import java.util.Collection;
+import java.util.Map;
 
 interface Parent {
 
     Collection<PluginDefinition> plugins();
 
     Collection<DependencyDefinition> dependencies();
+
+    Map<String, String> properties();
 }
