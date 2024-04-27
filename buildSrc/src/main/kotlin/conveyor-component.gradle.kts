fdf4c29b371bdfef51fde23ee12864ee795aa26c
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-04-27 13:11:04 +0200

    conveyor-component.gradle.kts

--- buildSrc/src/main/kotlin/conveyor-component.gradle.kts
+++ buildSrc/src/main/kotlin/conveyor-component.gradle.kts
@@ -5,38 +5,37 @@ plugins {
     `java-library`
 }
 
-tasks.withType(Jar::class.java) {
-    tasks.register<Copy>("installConveyorComponent") {
-        group = "distribution"
-        description = "Install the Conveyor component to a local Conveyor directory"
-        from(archiveFile)
-        val directory = project.group.toString()
-            .split(".")
-            .fold(project.rootDir.toPath().resolve(".conveyor-repository"), Path::resolve)
-            .resolve(project.name)
-            .resolve(project.version.toString())
-        into(directory)
-        dependsOn(this@withType)
-        doLast {
-            val dependencies = configurations
-                .compileClasspath
-                .get()
-                .resolvedConfiguration
-                .firstLevelModuleDependencies
-                .filter { !it.moduleName.contains("bom") }
-                .joinToString(",") {
-                    println(it)
-                    """
+tasks.register<Copy>("installConveyorComponent") {
+    group = "distribution"
+    description = "Install the Conveyor component to a local Conveyor directory"
+    val jarTask = tasks.withType(Jar::class.java).last()
+    from(jarTask.archiveFile)
+    val directory = project.group.toString()
+        .split(".")
+        .fold(project.rootDir.toPath().resolve(".conveyor-repository"), Path::resolve)
+        .resolve(project.name)
+        .resolve(project.version.toString())
+    into(directory)
+    dependsOn(jarTask)
+    doLast {
+        val dependencies = configurations
+            .compileClasspath
+            .get()
+            .resolvedConfiguration
+            .firstLevelModuleDependencies
+            .filter { !it.moduleName.contains("bom") }
+            .joinToString(",") {
+                """
                     {
                         "group": "${it.moduleGroup}",
                         "name": "${it.moduleName}",
                         "version": "${it.moduleVersion}"
                     }
                     """.trimIndent()
-                }
-            Files.writeString(
-                directory.resolve("${project.name}-${project.version}.json"),
-                """
+            }
+        Files.writeString(
+            directory.resolve("${project.name}-${project.version}.json"),
+            """
                 {
                     "group": "${project.group}",
                     "name": "${project.name}",
@@ -44,8 +43,6 @@ tasks.withType(Jar::class.java) {
                     "dependencies": [ $dependencies ]
                 }
                 """.trimIndent()
-            )
-        }
+        )
     }
 }
-
